be1109499ba75f7463553fa53d84bb2aeb591c1d
Argus <aathanas@datapotential.io>
2024-02-01 20:28:56 -0800

    reworked camp and added comprehensive tests

---
+++ colorcamp/_camp.py
@@ -0,0 +1,288 @@
+"""Camp organizes colors into frame work for projects"""
+
+from typing import Any, Union, Optional, Dict, Sequence
+from pathlib import Path
+import json
+from copy import copy
+
+from ._settings import settings
+from .color_objects._color_metadata import ColorInfo
+from .common.types import ColorObject, ColorSpace
+from .common.validators import PathValidator
+from .color_objects.color_space import BaseColor
+from .color_objects import Map, Scale, Palette
+
+ColorObjectType = Union[BaseColor, Scale, Palette, Map]
+
+# TODO:
+#   * HTML report
+#   * clean up saving code
+#   * Bucket names property
+
+
+class Bucket:
+    """A bucket for Color Objects"""
+
+    def __init__(self, bucket_type: ColorObjectType):
+        """A bucket for Color objects
+
+        Parameters
+        ----------
+        bucket_type : ColorObjectType
+            One of the key Color objects: Color, Scale, Palette, Map
+        """
+
+        self._bucket_type = bucket_type
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if __name == "_bucket_type":
+            # ? Validate
+            pass
+        elif not isinstance(__value, self._bucket_type):
+            raise TypeError(f"Colors must be of {self._bucket_type.__name__}")
+        elif __value.name != __name:
+            raise AttributeError(f'Names do not match: {__value.name}, {__name}')
+        super().__setattr__(__name, __value)
+
+    def __getitem__(self, value):
+        return self.__dict__[value]
+
+    def add(self, item: ColorObjectType):
+        """
+        Add an item of matching type to the bucket.
+
+        Parameters
+        ----------
+        item : Union[Color, Scale, Palette, Map]
+            Item to add to the
+
+        """
+
+        if (name := item.name) is None:
+            raise AttributeError(
+                f"Objects need to have a name to be added to a Camp {self.__class__.__name__} Bucket"
+            )
+        if hasattr(self, name):
+            raise ValueError(f"name '{name}' is already in use")
+        setattr(self, name, item)
+
+    def remove(self, name: str):
+        """Remove item from the bucket by name
+
+        Parameters
+        ----------
+        name : str
+            name of the item
+
+        """
+        if not hasattr(self, name):
+            raise KeyError(f"name '{name} is not in bucket")
+        delattr(self, name)
+
+    def to_dict(self) -> Dict[str, ColorSpace]:
+        """Return the bucket as a dictionary
+
+        Returns
+        -------
+        ColorSpace
+            Color object of the bucket type
+        """
+
+        bucket_dict = copy(self.__dict__)
+        bucket_dict.pop("_bucket_type")
+
+        return bucket_dict
+    
+    @property
+    def names(self):
+        return list(self.to_dict().keys())
+
+    def __repr__(self):
+        bucket_type = self._bucket_type.__name__
+        if bucket_type == 'BaseColor':
+            bucket_type = 'Color'
+        return f'{bucket_type}s{self.names}'
+
+
+class Camp(ColorInfo):
+    """A camp of colors!"""
+
+    def __init__(
+        self,
+        name: str,
+        description: Optional[str] = None,
+        metadata: Optional[Dict[str, Any]] = None,
+    ):
+        """A collection of colors and color objects. A Camp of Colors!
+
+        Parameters
+        ----------
+        name : Optional[str], optional
+            descriptive name, cannot contain special characters, by default None
+        description : Optional[str], optional
+            short descriptive text to provide additional context (max 255 char), by default None
+        metadata : Optional[Dict[str, Any]], optional
+            unstructured metadata used for querying and additional context, by default None
+        """
+
+        super().__init__(
+            name=name,
+            description=description,
+            metadata=metadata,
+        )
+
+        self._colors = Bucket(bucket_type=BaseColor)
+        self._scales = Bucket(bucket_type=Scale)
+        self._palettes = Bucket(bucket_type=Palette)
+        self._maps = Bucket(bucket_type=Map)
+
+    @property
+    def colors(self):
+        """Named camp colors"""
+        return self._colors
+
+    @property
+    def scales(self):
+        """Named camp scales"""
+        return self._scales
+
+    @property
+    def palettes(self):
+        """Named camp palettes"""
+        return self._palettes
+
+    @property
+    def maps(self):
+        """Named camp maps"""
+        return self._maps
+
+    @classmethod
+    def __directory_map(cls) -> Dict[str, ColorObjectType]:
+        dir_map: Dict[str, ColorObjectType] = {
+            "colors": BaseColor,
+            "scales": Scale,
+            "palettes": Palette,
+            "maps": Map,
+        }
+
+        return dir_map
+
+    def add_objects(self, color_objects:Sequence[ColorObject], exists_ok=False):
+        """Add any number of color objects to the Camp
+
+        Parameters
+        ----------
+        color_objects : Sequence[ColorObject]
+            A sequence of color objects
+        exists_ok : bool, optional
+            Ignore ValueErrors if conflicting names exist, by default False
+        """
+
+        map_directory = {klass.__name__:key for key, klass in self.__directory_map().items()}
+
+        for color_object in list(color_objects):
+            if isinstance(color_object, BaseColor):
+                co_type = 'BaseColor'
+            else:
+                co_type = color_object.__class__.__name__
+
+            bucket : Bucket = getattr(self, map_directory[co_type])
+            try:
+                bucket.add(color_object)
+            except ValueError as value_error:
+                if not exists_ok:
+                    raise value_error
+
+    @classmethod
+    def load(
+        cls, name: str, directory: Optional[Union[str, Path]] = None, color_type: Optional[ColorSpace] = None
+    ):
+        """Load a saved camp
+
+        Parameters
+        ----------
+        name : str
+            Name of the saved camp. This must match a subdirectory within `directory`
+        directory : Optional[Union[str, Path]], optional
+            Absolute or relative path to where camp is saved, by default None
+        color_type : ColorSpace, optional
+            The new color representation (Color subclass). If None is supplied the default representation is used, by default None
+
+        Returns
+        -------
+        Camp
+            A collection of colors and color objects
+
+        """
+
+        # Search for matching directory in source locations
+        if directory is None:
+            # use config to find if name is in any sources
+            raise NotImplementedError()
+        else:
+            directory = Path(directory)
+
+        if color_type is None:
+            color_type = settings.default_color_type
+
+        camp_dir = directory / name
+        PathValidator().validate(camp_dir)
+
+        with open(camp_dir / "camp_info.json", "r", encoding="utf-8") as fio:
+            camp_info: dict = json.load(fio)
+
+        camp = cls(**camp_info)
+
+        for sub_dir, klass in cls.__directory_map().items():
+            search_dir: Path = camp_dir / sub_dir
+            files = search_dir.glob("*.json") if search_dir.exists() else []
+            for file in files:
+                bucket: Bucket = getattr(camp, sub_dir)
+                bucket.add(klass.load_json(file, color_type=color_type))
+
+        return camp
+
+    def save(self, directory: Union[str, Path], overwrite=False):
+        """Save a camp
+
+        Parameters
+        ----------
+        directory : Union[str, Path]
+            Absolute or relative path to where camp is saved
+        overwrite : bool, optional
+            Overwrite files, by default False
+        """
+
+        PathValidator().validate(directory)
+        dest: Path = Path(directory) / self.name
+        dest.mkdir(exist_ok=True)
+
+        info_path = dest / "camp_info.json"
+        if info_path.exists() and not overwrite:
+            with open(info_path, mode = 'r', encoding="utf-8") as fio:
+                current = json.load(fio)
+            if current != self.info():
+                raise FileExistsError(f"file already exists: {info_path}")
+
+        with open(info_path, mode="w", encoding="utf-8") as fio:
+            json.dump(self.info(), fio, indent=4)
+
+        for sub_dir, _ in self.__directory_map().items():
+            sub_dest_dir: Path = dest / sub_dir
+            sub_dest_dir.mkdir(exist_ok=True)
+
+            for name, color_object in getattr(self, sub_dir).to_dict().items():
+                color_object_path = sub_dest_dir / f"{name}.json"
+
+                if color_object_path.exists() and not overwrite:
+
+                    current = color_object.load_json(color_object_path)
+
+                    if current != color_object:
+                        raise FileExistsError(f"file already exists: {color_object_path}")
+
+                    continue
+
+                color_object.dump_json(
+                    color_object_path, overwrite=overwrite
+                )
--- colorcamp/tests/test_camp.py
+++ colorcamp/tests/test_camp.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 
 import pytest
-from copy import copy
+from copy import deepcopy
 
 from colorcamp._camp import Camp
 from colorcamp.color_objects.color_space import BaseColor
@@ -10,12 +10,17 @@ from colorcamp.color_objects import Map, Scale, Palette
 from colorcamp.color_objects import Hex
 from colorcamp._settings import settings
 
+# TODO:
+# * split tests into more discrete cases
+
+
 @pytest.fixture(scope="class")
 def cls_camp(request):
     sky_hex: BaseColor = request.getfixturevalue("sky_Color").to_hex()
     pink_hex: BaseColor = request.getfixturevalue("pink_hex")
     mustard_hex: BaseColor = request.getfixturevalue("mustard_rgb").to_hex()
     lime_hex: BaseColor = request.getfixturevalue("lime_hsl").to_hex()
+    pal = Palette((sky_hex, pink_hex, mustard_hex, lime_hex), name="Pal")
 
     camp = Camp(
         name="TestCamp",
@@ -23,54 +28,93 @@ def cls_camp(request):
         metadata={"version": "0.1.0"},
     )
 
-    camp.colors.add(sky_hex)
-    camp.colors.add(pink_hex)
-    camp.colors.add(mustard_hex)
-    camp.colors.add(lime_hex)
-
-    camp.palettes.add(Palette((sky_hex, pink_hex, mustard_hex, lime_hex), name="Pal"))
-    camp.scales.add(Scale(camp.palettes.Pal, name="lette"))
-    camp.maps.add(Map({"mustard": mustard_hex, "lime": lime_hex}, name="GoodFood"))
+    camp.add_objects(
+        [
+            sky_hex,
+            pink_hex,
+            mustard_hex,
+            lime_hex,
+            pal,
+            Scale(pal, name="lette"),
+            Map({"mustard": mustard_hex, "lime": lime_hex}, name="GoodFood"),
+        ]
+    )
 
     request.cls.camp: Camp = camp
 
 
-@pytest.mark.usefixtures('cls_camp')
+@pytest.mark.usefixtures("cls_camp")
 class TestCamp:
     """testing Camp"""
 
     def test_save_and_load(self):
-        camp : Camp = self.camp
+        camp: Camp = self.camp
 
         with TemporaryDirectory() as tempdir:
             tempdir = Path(tempdir)
 
             camp.save(tempdir)
 
-            new_camp = camp.load('TestCamp', tempdir)
+            new_camp = camp.load("TestCamp", tempdir)
 
             assert new_camp.colors.pink_hex == camp.colors.pink_hex
             assert new_camp.palettes.Pal == camp.palettes.Pal
             assert new_camp.scales.lette == camp.scales.lette
 
+            # Try re saving
+            camp.save(tempdir)
+
+            with pytest.raises(FileExistsError):
+                camp._description = "blah blah blah"
+                camp.save(tempdir)
+
+            with pytest.raises(FileExistsError):
+                new_camp.colors.remove('pink_hex')
+                new_camp.colors.add(Hex('#FFFFFF', name = 'pink_hex'))
+                new_camp.save(tempdir)
+
+    def test_names(self):
+        assert len(self.camp.colors.names) == (len(self.camp.colors.__dict__)-1)
+
     def test_no_name(self):
         with pytest.raises(AttributeError):
-            self.camp.colors.add(Hex('#66FF66'))
+            self.camp.colors.add(Hex("#66FF66"))
 
     def test_name_in_use(self):
         with pytest.raises(ValueError):
-            self.camp.colors.add(Hex('#66FF66', name = 'pink_hex'))
+            self.camp.colors.add(Hex("#66FF66", name="pink_hex"))
 
-    def test_remove_co(self):
-        camp_copy = copy(self.camp)
-        camp_copy.colors.remove('pink_hex')
+    def test_remove_color_object(self):
+        camp_copy = deepcopy(self.camp)
+        camp_copy.colors.remove("pink_hex")
 
-        assert not hasattr(camp_copy.colors, 'pink_hex')
+        # Test we can't remove it again
+        with pytest.raises(KeyError):
+            camp_copy.colors.remove("pink_hex")
 
+        assert not hasattr(camp_copy.colors, "pink_hex")
 
+    def test_bucket_add_non_color_object(self):
+        with pytest.raises(AttributeError):
+            self.camp.colors.add("random")
 
+    def test_bucket_wrong_color_object(self):
+        with pytest.raises(TypeError):
+            self.camp.colors.add(Palette((Hex("#000"), Hex("#FFF")), name="pal"))
 
+    def test_incorrect_direct_assignment(self):
+        with pytest.raises(AttributeError):
+            nothing = Hex("#000", name="nothing")
+            self.camp.colors.Something = nothing
 
+    def test_equivalent_retrieval(self):
+        assert self.camp.colors.sky_Color is self.camp.colors["sky_Color"]
 
+    def test_adding_extra_redundant_items(self):
+        camp_copy = deepcopy(self.camp)
+        camp_copy.add_objects(
+            [Hex("#000", name="a"), Hex("#000", name="a")], exists_ok=True
+        )
 
-
+        with pytest.raises(ValueError):
+            camp_copy.add_objects([Hex("#000", name="a")])
