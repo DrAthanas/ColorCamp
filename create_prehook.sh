f13549674d040c22c91362e53a82775ca643758a
Argus <aathanas@datapotential.io>
2024-02-14 20:57:56 -0800

    updated CI and dev tools

--- create_prehook.sh
+++ create_prehook.sh
@@ -4,8 +4,9 @@ hook_path="./.git/hooks/pre-commit"
 
 if ! [ -f $hook_path ]; then
   echo "pre-commit does not exist."
-  echo "black ." > $hook_path &&
+  echo "black ./colorcamp" > $hook_path &&
   echo "mypy ./colorcamp" >> $hook_path &&
+  echo "isort ./colorcamp" >> $hook_path &&
   echo "python lint.py -p ./colorcamp/" >> $hook_path
   chmod +x .git/hooks/pre-commit
 fi
