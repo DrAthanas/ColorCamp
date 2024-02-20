2db084f6337fb5789f87afcdf5c0e6407096b86e
Argus <aathanas@datapotential.io>
2024-02-19 20:18:59 -0800

    Reverted to poormans precommit

--- create_prehook.sh
+++ create_prehook.sh
@@ -4,9 +4,10 @@ hook_path="./.git/hooks/pre-commit"
 
 if ! [ -f $hook_path ]; then
   echo "pre-commit does not exist."
-  echo "black ./colorcamp" > $hook_path &&
-  echo "mypy ./colorcamp" >> $hook_path &&
+  poetry run pre-commit install
+  echo "black ./colorcamp" >> $hook_path &&
   echo "isort ./colorcamp" >> $hook_path &&
-  echo "python lint.py -p ./colorcamp/" >> $hook_path
+  echo "mypy ./colorcamp" >> $hook_path &&
+  echo "poetry run python lint.py -p ./colorcamp/" >> $hook_path &&
   chmod +x .git/hooks/pre-commit
 fi
